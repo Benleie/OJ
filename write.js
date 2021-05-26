c94290fdbc03babd8740358787eca7e6ec6f148e
ben leie <awecg@qq.com>
2021-05-26 16:54:15 +0800

    <feat> remove duplicates from sorted array1

--- write.js
+++ write.js
@@ -4,7 +4,7 @@ let map1 = new Map([
   [true, 7, 2],
   [[1,2], [1]]
 ])
-console.log(map1)
+// console.log(map1)
 
 let obj = {
   name: 'hhh',
@@ -12,5 +12,23 @@ let obj = {
 }
 
 var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
-console.log(set)
-console.log(Array.from(set));
+// console.log(set)
+// console.log(Array.from(set));
+
+
+let rmDup = function(dupArr) {
+  let newArr = []
+  let index = 0
+  for(let i = 1; i < dupArr.length; i++) {
+    // 与后一项对比，看看是否重复
+    if(dupArr[index] !== dupArr[i])
+      // newArr.push(dupArr[i])
+      dupArr[++index] = dupArr[i]
+  }
+  // return 
+  return dupArr.slice(0, index + 1)
+}
+
+let arr11 = [1, 2, 2, 2, 3, 4, 4, 5]
+console.log(rmDup(arr11))
+
