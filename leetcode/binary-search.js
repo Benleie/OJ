9890a84c69f3219cbaef3b95d136064c32dd2822
ben leie <awecg@qq.com>
2021-05-27 19:53:35 +0800

    <feat> 34:find-first-and-last-position-of-element-in-sorted-array

--- leetcode/binary-search.js
+++ leetcode/binary-search.js
@@ -20,9 +20,6 @@ let arr = [0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6]
 // console.log(search(arr, 2))
 
 let binarySearch = function(nums, target) {
-  if(!target || !nums.length) 
-    return 'please input valid params'
-
   let left = 0,
       right = nums.length
   while(left <= right) {
@@ -55,5 +52,33 @@ let leftBound = function(nums, target) {
   return nums[left] == target ? left : -1
 }
 
-console.log(leftBound([0,1,1,2,2,3,3], 1))
-console.log(binarySearch([0,1,2,3,4,5], 3))
+// console.log(leftBound([0,1,1,2,2,3,3], 1))
+// console.log(binarySearch([0,1,2,3,4,5], 3))
+
+
+// 34:https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  let left = 0, right = nums.length
+  // find left boundary
+  while(left < right) {
+    let mid = Math.floor((left + right) / 2)
+    if(nums[mid] >= target) 
+      right = mid
+    else
+      left = mid + 1
+  }
+  if(nums[left] != target) return [-1,-1]
+  // get right boundary
+  for(let i = left; i < nums.length; i++) {
+    if(nums[i] != target)
+      return [left, i - 1]
+    else if(i == nums.length - 1)
+      return [left, i]
+  }
+};
+// console.log(searchRange([1,1], 1))
