0545b0ef627527936446e777bda77361c12ef3db
ben leie <awecg@qq.com>
2021-05-27 11:28:31 +0800

    <feat> remove duplicates from sorted arrayii

--- leetcode/remove-duplicates-from-sorted-array.js
+++ leetcode/remove-duplicates-from-sorted-array.js
@@ -1,3 +1,5 @@
+
+// 
 /**
  * @param {number[]} nums
  * @return {number}
@@ -12,5 +14,29 @@ var removeDuplicates = function(nums) {
   return index + 1
 };
 
-let arr = [1, 1, 2, 3, 3, 4]
-console.log(removeDuplicates(arr))
+//  https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+
+var removeDuplicates2 = function(nums) {
+  let index = 0;
+  let count = 1;
+  for(let i = 1; i < nums.length; i++) {
+    if(nums[index] !== nums[i]) {
+      nums[++index] = nums[i]
+      count = 1
+    } 
+    else {
+      if(count < 2) {
+        nums[++index] = nums[i]
+      }
+      count++
+    }
+  }
+  nums.splice(index + 1)
+  // console.log(nums)
+  return index + 1
+};
+
+
+
+let arr = [1, 1, 1, 2, 3, 3, 3, 3, 4]
+console.log(removeDuplicates2([0,0,1,1,1,1,1,2,3,3,3]))
