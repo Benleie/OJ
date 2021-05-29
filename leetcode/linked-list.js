3d9479351a982e421526cc09a777a2c098217030
ben leie <awecg@qq.com>
2021-05-29 21:34:11 +0800

    <feat> 141: linked-list-cycle

--- leetcode/linked-list.js
+++ leetcode/linked-list.js
@@ -177,9 +177,50 @@ var addTwoNumbers = function(l1, l2) {
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    
+  const prehead = new ListNode(-1);
+
+  let prev = prehead;
+  while (l1 != null && l2 != null) {
+      if (l1.val <= l2.val) {
+          prev.next = l1;
+          l1 = l1.next;
+      } else {
+          prev.next = l2;
+          l2 = l2.next;
+      }
+      prev = prev.next;
+  }
+
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = l1 === null ? l2 : l1;
+
+  return prehead.next;
 };
 
+
+// 141: https://leetcode-cn.com/problems/linked-list-cycle/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let map = new Map()
+    while(head) {
+      if(map.has(head)) 
+        return true
+      map.set(head, true)
+      head = head.next
+    }
+    return false
+};
 
 const linkedList1 = new LinkedList();
 linkedList1.append(1);
