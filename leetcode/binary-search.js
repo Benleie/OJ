78a32d1d39e250c1b6925763e52b77b1ab65a3e8
ben leie <awecg@qq.com>
2021-05-29 09:47:25 +0800

    <feat> 33:search-in-rotated-sorted-array 二分法

--- leetcode/binary-search.js
+++ leetcode/binary-search.js
@@ -82,3 +82,25 @@ var searchRange = function(nums, target) {
   }
 };
 // console.log(searchRange([1,1], 1))
+
+// 33:https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+var searchRotated = function(nums, target) {
+  let left = 0, right = nums.length;
+  while(left != right) {
+    let mid = Math.floor((left + right) / 2)
+    if(nums[mid] == target)
+      return mid
+    if(nums[left] <= nums[mid]) {
+      if(nums[left] <= target && target < nums[mid])
+        right = mid
+      else 
+        left = mid + 1
+    } else {
+      if(nums[mid] < target && target <= nums[right - 1])
+        left = mid + 1
+      else right = mid
+    }
+  }
+  return -1
+}
+console.log(searchRotated([4,5,6,7,8,9,10,1,2,3], 1))
