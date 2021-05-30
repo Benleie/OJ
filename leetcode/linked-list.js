1edb3f30723cfb001fddb7100938683cd1f619dd
ben leie <awecg@qq.com>
2021-05-30 09:52:03 +0800

    <feat> 234: palindrome-linked-list

--- leetcode/linked-list.js
+++ leetcode/linked-list.js
@@ -258,3 +258,29 @@ linkedList1.append(3);
 linkedList1.append(4);
 console.log(removeNthFromEnd(linkedList1.head, 3))
 // console.log(linkedList1.head)
+
+
+// 234: https://leetcode-cn.com/problems/palindrome-linked-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  let arr = []
+  while(head) {
+    arr.push(head.val)
+    head = head.next
+  }
+  for(let i = 0, j = arr.length - 1; i < j; i++, j--) {
+    if(arr[i] !== arr[j])
+      return false
+  }
+  return true
+};
