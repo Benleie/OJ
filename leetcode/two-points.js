a0655d11780649d1b8e0235cf0c1c6a1900e1e00
ben leie <awecg@qq.com>
2021-06-01 15:32:27 +0800

    <feat> 15: 3sum

--- leetcode/two-points.js
+++ leetcode/two-points.js
@@ -161,3 +161,36 @@ var maxArea = function(height) {
   return max
 };
 // console.log(maxArea([1,8,6,2,5,4,8,3,7]))
+
+/**
+ * 15: 3sum
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  if(!nums || nums.length < 3) return []
+  let arr = []
+  nums.sort((a,b) => a - b)
+  for(let i = 0; i < nums.length && nums[i] <= 0; i++) {
+    // 跳过重复元素
+    if(i > 0 && nums[i] === nums[i - 1]) continue
+    let left = i + 1, right = nums.length - 1
+    while(left < right) {
+      if(nums[i] + nums[left] + nums[right] === 0) {
+        arr.push([nums[i], nums[left], nums[right]])
+        // 跳过重复元素
+        while(left < right && nums[left] === nums[left + 1])
+          left++
+        while(left < right && nums[right] === nums[right - 1])
+          right--
+        left++, right--;
+      } else if(nums[i] + nums[left] + nums[right] < 0) {
+        left++
+      } else if(nums[i] + nums[left] + nums[right] > 0) {
+        right--
+      }
+    }
+  }
+  return arr
+};
+// console.log(threeSum([-1,0,1,2,-1,-4]))
