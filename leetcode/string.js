a934ba32993931ef7e1f41f1a2c020b5f133dbfb
ben leie <awecg@qq.com>
2021-06-23 08:48:46 +0800

    <review> decode-string

--- leetcode/string.js
+++ leetcode/string.js
@@ -1,3 +1,4 @@
+const log = console.log
 /**
  * https://leetcode-cn.com/problems/first-unique-character-in-a-string
  * @param {string} s
@@ -85,3 +86,85 @@ var reverseWords = function(s) {
 var commonChars = function(words) {
   
 };
+
+
+/**
+ * https://leetcode-cn.com/problems/long-pressed-name/
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+  let k = 0;
+  let prev = ''
+  for(let i = 0; i < typed.length; i++) {
+    if(typed[i] === name[k]) {
+      prev = name[k++]
+    } 
+    else if(typed[i] === prev) continue
+    else return false
+  }
+  return k === name.length ? true : false
+};
+// console.log(isLongPressedName('alexd', 'ale'))
+
+
+
+
+/**
+ * https://leetcode-cn.com/problems/next-greater-element-i/submissions/
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+  const arr = Array(nums1.length).fill(-1)
+  for(let j = 0; j < nums1.length; j++) {
+      const k = nums2.indexOf(nums1[j])
+      for(let i = k + 1; i < nums2.length; i++) {
+          if(nums2[i] > nums1[j]) {
+            arr[j] = nums2[i]
+            break
+          } 
+      }
+  }
+  return arr
+};
+// log(nextGreaterElement([4,1,2], [1,3,4,2]))
+
+
+
+
+/**
+ * https://leetcode-cn.com/problems/decode-string/
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let count = 0;
+    let str = ''
+    let res = ''
+    for(let i = 0; i < s.length; i++) {
+      const word = s[i]
+      if(!isNaN(Number(word))) {
+        count = 10 * count + Number(word)
+      }
+      else if(word === '[') {
+        const resStr = decodeString(s.slice(s.indexOf(word) + 1))
+        str += resStr
+        i += resStr.length + 1
+      }
+      else if(word === ']') return str 
+      else {
+        str += word
+        log(`str is ${str}`)
+      }
+      
+    }
+    while(count > 0) {
+      res += str
+      count--
+    }
+    return res
+};
+log(decodeString('3[mn2[k]]'))
