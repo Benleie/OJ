d96c7457954e556559e5f59fd78e26321bfa3cd0
ben leie <awecg@qq.com>
2021-06-27 17:08:16 +0800

    <improve> next-greater-element-i 单调栈

---
+++ leetcode/stack.js
@@ -0,0 +1,73 @@
+const log = console.log
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+ 
+var isValid = function(s) {
+  let stringArr = s.split('')
+  let stackArr = [];
+  
+  for(value of stringArr) {
+    switch(value){
+      case "(":
+        stackArr.push(value)
+        break
+      case "[":
+        stackArr.push(value)
+        break
+      case '{':
+        stackArr.push(value)
+        break
+      case ")":
+        if(stackArr.pop() !== "(") return false
+        break
+      case "]":
+        if(stackArr.pop() !== '[') return false
+        break
+      case '}':
+        if(stackArr.pop() !== '{') return false
+        break
+    }
+  }
+  return !stackArr.length
+};
+
+console.log(isValid("(([]){})"))
+// console.log(isValid("([)])"))
+
+// 速度慢，内存占用高
+let isValid2 = (str) => {
+  str = str.replace(/\s*/g, '')
+  while(str.includes('()') || str.includes('[]') || str.includes('{}')) {
+    str = str.replace('()', '')
+    str = str.replace('[]', '')
+    str = str.replace('{}', '')
+  }
+  return str === ''
+}
+
+/**
+ * https://leetcode-cn.com/problems/next-greater-element-i/submissions/
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+  const arr = []
+  const stack = []
+  const map = new Map()
+  for(let i = 0; i < nums2.length; i++) {
+    while(stack.length && stack[stack.length - 1] < nums2[i]) {
+      map.set(stack.pop(), nums2[i])
+    }
+    stack.push(nums2[i])
+  }
+  for(let i = 0; i < nums1.length; i++) {
+    arr[i] = map.get(nums1[i]) || -1
+  }
+  return arr
+}
+// log(nextGreaterElement([4,1,2], [1,3,4,2]))
