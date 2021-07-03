b976e64340ddad80515bc603b5aa0cd36bb66629
ben leie <awecg@qq.com>
2021-07-03 11:51:17 +0800

    <doc> 整理有序数组去重

--- leetcode/two-points.js
+++ leetcode/two-points.js
@@ -1,3 +1,47 @@
+const log = console.log
+
+/**
+ * 26 https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let index = 0;
+  for(let i = 1; i < nums.length; i++) {
+    if(nums[index] !== nums[i]) 
+      nums[++index] = nums[i]
+  }
+  nums.splice(index + 1)
+  return index + 1
+};
+
+//  https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+
+var removeDuplicates2 = function(nums) {
+  let index = 0;
+  let count = 1;
+  for(let i = 1; i < nums.length; i++) {
+    if(nums[index] !== nums[i]) {
+      nums[++index] = nums[i]
+      count = 1
+    } 
+    else {
+      if(count < 2) {
+        nums[++index] = nums[i]
+      }
+      count++
+    }
+  }
+  nums.splice(index + 1)
+  // console.log(nums)
+  return index + 1
+};
+
+// let arr = [1, 1, 1, 2, 3, 3, 3, 3, 4]
+// console.log(removeDuplicates2([0,0,1,1,1,1,1,2,3,3,3]))
+
+
+
 /**
  * move-zeroes
  * @param {number[]} nums
@@ -34,8 +78,8 @@ var moveZeroes = function(nums) {
 }
 
 
-let arr3 = [0,1,2,3,4]
-moveZeroes([0,0,1])
+// let arr3 = [0,1,2,3,4]
+// moveZeroes([0,0,1])
 
 // reverse-string
 /**
@@ -53,6 +97,8 @@ var reverseString = function(s) {
       right--
   }
 };
+
+
 
 /**
  * longest-substring-without-repeating-characters
