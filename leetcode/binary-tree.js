f2db28c2e3ab3b73bb117419601b4cd297e4f4b7
ben leie <awecg@qq.com>
2021-07-08 09:22:56 +0800

    <feat> merge-two-binary-trees

--- leetcode/binary-tree.js
+++ leetcode/binary-tree.js
@@ -217,3 +217,18 @@ var diameterOfBinaryTree = function(root) {
       return Math.max(left, right) + 1
   }
 };
+
+/**
+ * https://leetcode-cn.com/problems/merge-two-binary-trees/submissions/
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(root1, root2) {
+  if(!root1) return root2
+  if(!root2) return root1
+  let newNode = new TreeNode(root1.val + root2.val)
+  newNode.left = mergeTrees(root1.left, root2.left)
+  newNode.right = mergeTrees(root1.right, root2.right)
+  return newNode
+};
