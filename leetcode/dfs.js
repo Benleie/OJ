e18596416db85a15e072d5d7570ce0c30faffdce
ben leie <awecg@qq.com>
2021-07-11 19:19:44 +0800

    <preview> average-of-levels-in-binary-tree

--- leetcode/dfs.js
+++ leetcode/dfs.js
@@ -89,3 +89,32 @@ var isSubtree = function(root, subRoot) {
     return check(s.left, t.left) && check(s.right, t.right)
   }
 };
+
+
+
+
+/**
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+  //层级平均值
+  let res=[],queue=[];
+  queue.push(root);
+  while(queue.length&&root!==null){
+      //每一层节点个数
+      let length=queue.length;
+      //sum记录每一层的和
+      let sum=0;
+      for(let i=0;i<length;i++){
+          let node=queue.shift();
+          sum+=node.val;
+          node.left&&queue.push(node.left);
+          node.right&&queue.push(node.right);
+      }
+      //每一层的平均值存入数组res
+      res.push(sum/length);
+  }
+  return res;
+};
