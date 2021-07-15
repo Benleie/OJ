4923649b68abf592dab782e4f45a541a0f2db1ec
ben leie <awecg@qq.com>
2021-07-15 19:53:51 +0800

    <feat> construct-binary-tree-from-preorder-and-inorder-traversal

--- leetcode/dfs.js
+++ leetcode/dfs.js
@@ -259,3 +259,52 @@ var connect = function(root) {
       connectTwoNodes(node1.right, node2.left)
     }
 };
+
+
+
+
+
+/**
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+const buildTree = (preorder, inorder) => {
+  if (inorder.length == 0) return null;
+  const root = new TreeNode(preorder[0]);
+  const mid = inorder.indexOf(preorder[0]);
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+  return root;
+};
+
+
+
+const buildTree = (preorder, inorder) => {
+  if (inorder.length == 0) return null;
+  const root = new TreeNode(preorder[0]);
+  const mid = inorder.indexOf(preorder[0]);
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+  return root;
+};
+
+/* const buildTree = (preorder, inorder) => {
+  const map = new Map();
+  for (let i = 0; i < inorder.length; i++) {
+    map.set(inorder[i], i);
+  }
+  const helper = (p_start, p_end, i_start, i_end) => {
+    if (p_start > p_end) return null;
+    let rootVal = preorder[p_start];    // 根节点的值
+    let root = new TreeNode(rootVal);   // 根节点
+    let mid = map.get(rootVal);         // 根节点在inorder的位置
+    let leftNum = mid - i_start;        // 左子树的节点数
+    root.left = helper(p_start + 1, p_start + leftNum, i_start, mid - 1);
+    root.right = helper(p_start + leftNum + 1, p_end, mid + 1, i_end);
+    return root;
+  };
+  return helper(0, preorder.length - 1, 0, inorder.length - 1);
+}; */
